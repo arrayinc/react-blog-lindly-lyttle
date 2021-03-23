bcadadbcd79fc0ed097774806f79460c8c9819aa
Abby Lyttle <paige.abby@gmail.com>
2021-03-23 16:23:49 -0600

    added bootstrap

---
+++ break/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import 'react-bootstrap'
+import Carousel from './carousel';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+       <h1>Emily and Abby</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
