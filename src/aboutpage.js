aa699832824edd1ff9cc4f2b3a743c72dff62110
Abby Lyttle <paige.abby@gmail.com>
2021-03-25 16:56:28 -0600

    added some link functioning

--- src/aboutpage.js
+++ src/aboutpage.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 function AboutPage() {
   return (
     <>
     <div>
-    <h1>About Page</h1>
+    <h1>About The Authors</h1>
+    <Link to="/">return to home</Link>
     </div>
     </>
   );
