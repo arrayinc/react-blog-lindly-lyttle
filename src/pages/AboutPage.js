660ff0ae78cd708b6ccff5d450dac425448f135f
Abby Lyttle <paige.abby@gmail.com>
2021-03-26 15:26:48 -0600

    unfinished changes

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import Image from 'react-bootstrap/Image';
-import { Container, Row, Col, } from 'react-bootstrap';
+import { Container, Row, Col, Button, Overlay, } from 'react-bootstrap';
 import  PhotoOne  from '../images/carousel/Calm1.jpg';
 import PhotoTwo from '../images/carousel/Calm2.jpg';
 import PhotoThree from '../images/carousel/Calm3.jpg';
 
 
 function AboutPage() {
-    return (
+  const [show, setShow] = useState(false);
+  const target = useRef(null);
+    
+  return (
       <div>
     <h1>About the Authors</h1>
     <Container>
   <Row>
     <Col xs={6} md={4}>
-      <Image src={PhotoOne} roundedCircle fluid />
+      <Image src={PhotoOne} ref={target} onClick={() => setShow(!show)} roundedCircle fluid />
+      {/* <Button variant="danger" ref={target} onClick={() => setShow(!show)}>
+        Click me to see
+      </Button> */}
+      <Overlay target={target.current} show={show} placement="right">
+        {({ placement, arrowProps, show: _show, popper, ...props }) => (
+          <div
+            {...props}
+            style={{
+              backgroundColor: 'rgba(255, 100, 100, 0.85)',
+              padding: '2px 10px',
+              color: 'white',
+              borderRadius: 3,
+              ...props.style,
+            }}
+          >
+            Author Info
+          </div>
+        )}
+      </Overlay>
     </Col>
     <Col xs={6} md={4}>
       <Image src={PhotoTwo} roundedCircle fluid />
@@ -23,6 +45,27 @@ function AboutPage() {
     </Col>
   </Row>
 </Container>
+
+
+{/* <Button variant="danger" ref={target} onClick={() => setShow(!show)}>
+        Click me to see
+      </Button> */}
+      <Overlay target={target.current} show={show} placement="right">
+        {({ placement, arrowProps, show: _show, popper, ...props }) => (
+          <div
+            {...props}
+            style={{
+              backgroundColor: 'rgba(255, 100, 100, 0.85)',
+              padding: '2px 10px',
+              color: 'white',
+              borderRadius: 3,
+              ...props.style,
+            }}
+          >
+            Author Info
+          </div>
+        )}
+      </Overlay>
     </div>
     
   );
