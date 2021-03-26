c0adf56ed7c2131be4ee5c8629ea117751b7300e
Abby Lyttle <paige.abby@gmail.com>
2021-03-26 16:22:24 -0600

    added overlay text popouts to images. Added about author files.

---
+++ src/pages/AboutAuthor3.js
@@ -0,0 +1,34 @@
+import React, { useState, useRef } from 'react';
+import Image from 'react-bootstrap/Image';
+import {  Row, Col, Overlay, } from 'react-bootstrap';
+import  PhotoThree  from '../images/carousel/Calm3.jpg';
+
+function AboutAuthor3() {
+    const [show, setShow] = useState(false);
+    const target = useRef(null);
+      
+    return (
+    
+    <Row>
+      <Col xs={6} md={4}>
+        <Image src={PhotoThree} ref={target} onClick={() => setShow(!show)} roundedCircle fluid />
+        <Overlay target={target.current} show={show} placement="bottom">
+          {({ placement, arrowProps, show: _show, popper, ...props }) => (
+            <div
+              {...props}
+              style={{
+                backgroundColor: 'rgba(255, 100, 100, 0.85)',
+                padding: '2px 10px',
+                color: 'white',
+                borderRadius: 3,
+                ...props.style,
+              }}
+            >
+              Author Info
+            </div>
+          )}
+         </Overlay>
+    </Col>
+  </Row>
+    )}
+    export default AboutAuthor3;
