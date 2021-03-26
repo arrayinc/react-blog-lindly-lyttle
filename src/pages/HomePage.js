f081d97956ceeb536f2874bd32dccb9d16ad0c4d
Abby Lyttle <paige.abby@gmail.com>
2021-03-26 14:50:06 -0600

    added about author image circles

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { CarouselPhotos } from '../carousel';
 import { CardsForBlogs } from '../components/cards';
+import  AboutPhotos  from './AboutPage';
 
 function HomePage() {
   return (
     <div>
       <CarouselPhotos />
+      <AboutPhotos />
       <CardsForBlogs />
     </div>
   );
