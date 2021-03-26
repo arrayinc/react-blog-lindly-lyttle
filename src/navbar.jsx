b0d35e6ec47fd1bd8bfa69118788ac6c549704a4
Abby Lyttle <paige.abby@gmail.com>
2021-03-25 18:46:03 -0600

    added icon, about functioning in Navbar and bootstrap layout for about authors

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -6,20 +6,20 @@ import {
   Form,
   FormControl,
   Button,
-  Icon,
 } from "react-bootstrap";
+import { PauseCircle } from 'react-bootstrap-icons';
 
 export class SiteNavbar extends Component {
   render() {
     return (
       <div>
         <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="#home">Break<i class="bi bi-pause-circle"></i></Navbar.Brand>
+          <Navbar.Brand href="#home">Break<PauseCircle /> </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
               <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
               <NavDropdown title="Blog" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Blog 1</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,10 +1,24 @@
 import React from 'react';
-
+import Image from 'react-bootstrap/Image';
+import { Container, Row, Col, } from 'react-bootstrap';
 function AboutPage() {
   return (
     <>
     <div>
-    <h1>About Page</h1>
+    <h1>About the Authors</h1>
+    <Container>
+  <Row>
+    <Col xs={6} md={4}>
+      <Image src="holder.js/171x180" rounded />
+    </Col>
+    <Col xs={6} md={4}>
+      <Image src="holder.js/171x180" roundedCircle />
+    </Col>
+    <Col xs={6} md={4}>
+      <Image src="holder.js/171x180" thumbnail />
+    </Col>
+  </Row>
+</Container>
     </div>
     </>
   );
