e0befdbcfdd3388210905213d1f35d94f6786deb
Emily Lindly <emilylindly@gmail.com>
2021-03-27 12:15:53 -0600

    fixed merged conflicts on overall styling.

--- src/pages/WorkBreakPage.js
+++ src/pages/WorkBreakPage.js
@@ -1,15 +1,65 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import WorkBreakPhoto from "../images/WorkBreakPage/workbreakcover.jpg";
-import { Image } from "react-bootstrap";
+// import WorkBreakPhoto from "../images/WorkBreakPage/workbreakcover.jpg";
+// import { Image } from "react-bootstrap";
+import { Card } from "react-bootstrap";
+import { JumbotronForBlogs } from '../components/JumbotronBlogPages';
+import { Facebook, Twitter, Instagram } from "react-bootstrap-icons";
 
 function WorkBreak() {
   return (
     <>
+      <JumbotronForBlogs />
+      <h1>Work Break Blog Title</h1>
+      <h6>Authored By: | 3 min read | <Facebook /> {"  "} <Twitter /> {"  "} <Instagram /> {"  "} </h6>
+      <div className="blog-container"> </div>
       <div>
-        <h1>Work Break Blog Title</h1>
-        <Image src={WorkBreakPhoto} fluid />
-        <Link to="/">Let's go OM</Link>
+        <Card border="primary" style={{ width: '18rem' }}>
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>Primary Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk
+              of the card's content.
+      </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+
+        <Card border="secondary" style={{ width: '18rem' }}>
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>Secondary Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk
+              of the card's content.
+      </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+
+        <Card border="success" style={{ width: '18rem' }}>
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>Success Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk
+              of the card's content.
+      </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+
+        <Card border="info" style={{ width: '18rem' }}>
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>Info Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk
+              of the card's content.
+      </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
       </div>
     </>
   );
